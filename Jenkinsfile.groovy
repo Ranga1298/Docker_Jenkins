1599279ab5235897ca4a077168f499673463fe11
Ranga1298 <37245334+Ranga1298@users.noreply.github.com>
2023-10-09 10:03:12 -0400

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+
+    environment {
+        // Define your Docker image and tag
+        imageName = "ranga1298/docker_jenkins"
+        imageTag = "latest"
+        dockerFile = "Dockerfile"
+    }
+
+    stages {
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    // Build the Docker image
+                    sh "docker build -t ${imageName}:${imageTag} -f ${dockerFile} ."
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    // Log in to Docker Hub (you can replace this with your registry)
+                    sh "docker login -u ranga1298 -p April@12345"
+
+                    // Push the Docker image to the registry
+                    sh "docker push ${imageName}:${imageTag}"
+
+                    // Log out from Docker Hub
+                    sh "docker logout"
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Clean up by removing the local Docker image
+            sh "docker rmi ${imageName}:${imageTag}"
+        }
+    }
+}
