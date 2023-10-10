c1f351d20ffc6f5fb28b3e8ae106f2d34f80e922
Ranga1298 <37245334+Ranga1298@users.noreply.github.com>
2023-10-10 10:30:46 -0400

    Update hello-world.js

--- app1/hello-world.js
+++ app1/hello-world.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/helloworld', (req, res) => {
-  res.send("This is praneeth's World!")
+  res.send("Hello World!")
 })
 
 app.listen(port, () => {
-  console.log(`praneeth app listening on port ${port}`)
+  console.log(`ranga app listening on port ${port}`)
 })
